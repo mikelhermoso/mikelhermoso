bf510d84713a8af23a94c2d28dfd496b2703436a
mikel <mikel@creative>
2017-05-05 09:42:43 +0200

    proyecto 05/05/2017

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <html>
     <head>
         <title>Mikel Hermoso</title>
+        <LINK REL=StyleSheet HREF="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet"> 
     </head>
+    
+    <header>
+        <?php include 'layouts/header.php';?>
+    </header>
+    
     <body>
         <h1>Mikel Hermoso</h1>
     </body>
+    
+    <footer>
+       <?php include 'layouts/footer.php';?> 
+    </footer>
 </html>
