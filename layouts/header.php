a44a69b07e53344db1b527a7c996392323b1be57
Mikel <Mikel@DESKTOP-MR5LNSP>
2017-05-09 22:49:03 +0200

    09-05-2017 Mikel Pc

--- layouts/header.php
+++ layouts/header.php
@@ -9,7 +9,7 @@
         </ul>
     </nav>
     <div id="contentRedes">
-        <img class="imgRedes" src="img/redes/twitter.png" alt="facebook" />
+        <img class="imgRedes" src="img/redes/twitter.png" alt="twitter" />
         <img class="imgRedes" src="img/redes/youtube.png" alt="youtube" />
         <img class="imgRedes" src="img/redes/linkedin.png" alt="linkedin" />
     </div>
--- layouts/redesSociales.php
+++ layouts/redesSociales.php
@@ -2,16 +2,19 @@
     <div id="containerRedesSociales">
         <h1 class="titRedes">Sígueme!</h1>
         <div id="contentRedesIn">
-            <h3 class="titRedesIn">LINKEDIN</h3>
+            <h3 class="titRedesIn"><span class="colorLinkedin">LINKEDIN</span></h3>
+            <img class="imgRedesIn" src="img/redes/linkedin.png" alt="linkedin" />
             <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
 <script type="IN/MemberProfile" data-id="https://www.linkedin.com/in/mikelhermoso" data-format="inline" data-related="false"></script>
         </div>
         <div id="contentRedesIn">
-            <h3 class="titRedesIn">YOUTUBE</h3>
+            <h3 class="titRedesIn"><span class="colorYoutube">YOUTUBE</span></h3>
+            <img class="imgRedesIn" src="img/redes/youtube.png" alt="youtube" />
             <iframe width="380" height="270px" src="https://www.youtube.com/embed/Prog_QOFEy0" frameborder="0" allowfullscreen></iframe>                   
         </div>
         <div id="contentRedesIn">
-            <h3 class="titRedesIn">TWITTER</h3>
+            <h3 class="titRedesIn"><span class="colorTwitter">TWITTER</span></h3>
+            <img class="imgRedesIn" src="img/redes/twitter.png" alt="twitter" />
             <a class="twitter-timeline" data-width="380" data-height="270" href="https://twitter.com/TwitterDev">Tweets by TwitterDev</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
         </div>
     </div>
